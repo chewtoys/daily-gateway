db28a065212aeab60f061e23c59ddd8d1c9972cc
Ido Shamun <ido@elegantmonkeys.com>
2020-01-09 18:09:45 +0200

    refactor(users): change image column to text

---
+++ migrations/20200109180259_image_to_text.js
@@ -0,0 +1,9 @@
+exports.up = knex =>
+  knex.schema.table('users', (table) => {
+    table.text('image').alter();
+  });
+
+exports.down = knex =>
+  knex.schema.table('users', (table) => {
+    table.string('image').alter();
+  });
